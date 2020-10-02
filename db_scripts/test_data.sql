28f5663d6f406a27203d0c2fea95c8f6730a6bbd
samanderson4830 <50926328+samanderson4830@users.noreply.github.com>
2020-10-02 17:16:38 -0400

    added test_data sql script

---
+++ db_scripts/test_data.sql
@@ -0,0 +1,7 @@
+call My_Database.AddProduct('Product 0', 'This is a product description', 11.99, 10);
+call My_Database.AddProduct('Product 1', 'This is a product description', 10.99, 10);
+call My_Database.AddProduct('Product 2', 'This is a product description', 12.99, 10);
+call My_Database.AddProduct('Product 3', 'This is a product description', 14.99, 10);
+call My_Database.AddProduct('Product 4', 'This is a product description', 10.99, 10);
+call My_Database.AddProduct('Product 5', 'This is a product description', 20.99, 10);
+call My_Database.AddProduct('Product 6', 'This is a product description', 9.99,  10);
