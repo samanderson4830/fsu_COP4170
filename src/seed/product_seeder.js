ccbf13facad1bcb99f2f7e8588ce51e603daeed0
samanderson4830 <samanderson302@gmail.com>
2020-10-02 21:30:28 -0400

    added default images; updated login page

--- src/seed/product_seeder.js
+++ src/seed/product_seeder.js
@@ -17,10 +17,30 @@ const get_products = (req, res) => {
 
         } else {
 
+            sql = 'call My_Database.NumberOfProducts(@total);'
+            db.start.query(sql, (err, results) => {
+                if (err) {
+
+                    throw err;
+
+                } else {
+
+                    let total;
+                    console.log("Total: " + results[0][0].total)
+
+                    for (key in results){
+                        total = results[0][0].total;
+                        console.log(results[key]);
+                    }
+                    
+
+                }
+            });
+
             // populate the prodcuts array with database   
             // update 7 to be total number of products from db
             // instead of being hard coded     
-            for (let inx = 0; inx < 7; inx++) {
+            for (let inx = 0; inx < 21; inx++) {
 
                 products.push({
                     product_name: results[0][inx].product_name,
@@ -28,14 +48,14 @@ const get_products = (req, res) => {
                     price: results[0][inx].price
                 });
 
-        
 
-                console.log("Product ID:          " + results[0][inx].product_ID);
-                console.log("Product Name:        " + results[0][inx].product_name);
-                console.log("Product Description: " + results[0][inx].product_description);
-                console.log("Price:               " + results[0][inx].price);
-                console.log("Quantity:            " + results[0][inx].quantity);
-                console.log("-------------------------------");
+
+                 console.log("Product ID:          " + results[0][inx].product_ID);
+                 console.log("Product Name:        " + results[0][inx].product_name);
+                 console.log("Product Description: " + results[0][inx].product_description);
+                 console.log("Price:               " + results[0][inx].price);
+                 console.log("Quantity:            " + results[0][inx].quantity);
+                 console.log("-------------------------------");
 
             }
 
