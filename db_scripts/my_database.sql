a966b0bf2fa4d02684e78de2afa90647ac134840
samanderson4830 <50926328+samanderson4830@users.noreply.github.com>
2020-10-27 16:10:21 -0400

    added new tables and sp

--- db_scripts/my_database.sql
+++ db_scripts/my_database.sql
@@ -2,12 +2,17 @@ DROP SCHEMA IF EXISTS My_Database;
 CREATE SCHEMA My_Database;
 USE `My_Database`;
 
+# reset tables 
 DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `admin`;
 DROP TABLE IF EXISTS `products`;
 DROP TABLE IF EXISTS `cart`;
+DROP TABLE IF EXISTS `cart_has`;
+DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `placed_order`;
+DROP TABLE IF EXISTS `day_avaliable`;
 
 # customer/user information Table
-
 CREATE TABLE IF NOT EXISTS `My_Database`.`users` 
 (
   `email`         VARCHAR(100) NOT NULL,
@@ -18,8 +23,22 @@ CREATE TABLE IF NOT EXISTS `My_Database`.`users`
   PRIMARY KEY (`email`)
 )ENGINE = InnoDB;
 
-# all products 
+# admin table
+CREATE TABLE IF NOT EXISTS `My_Database`.`admin` 
+(
+  `email`         VARCHAR(100) NOT NULL,
+  `user_password` VARCHAR(500) NOT NULL,
+  `address`       VARCHAR(100) NOT NULL,
+  `ig_link`       VARCHAR(100) NOT NULL,
+  `fb_link`       VARCHAR(100) NOT NULL,
+  `twitter_link`  VARCHAR(100) NOT NULL,
+  `about`         VARCHAR(1000) NOT NULL,
+  `phone_number`  VARCHAR(40),
+  `date_time`     TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP, 
+  PRIMARY KEY (`email`)
+)ENGINE = InnoDB;
 
+# all products 
 CREATE TABLE IF NOT EXISTS `My_Database`.`products` 
 (
   `product_ID`         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
@@ -31,7 +50,6 @@ CREATE TABLE IF NOT EXISTS `My_Database`.`products`
 )ENGINE = InnoDB;
 
 # Table for who cart belongs to 
-
 CREATE TABLE IF NOT EXISTS `My_Database`.`cart` 
 (
   `cart_ID`       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
@@ -43,7 +61,6 @@ CREATE TABLE IF NOT EXISTS `My_Database`.`cart`
 )ENGINE = InnoDB;
 
 # Table for items found in cart
-
 CREATE TABLE IF NOT EXISTS `My_Database`.`cart_has` 
 (
   `cart_ID`       BIGINT UNSIGNED NOT NULL,
@@ -57,9 +74,41 @@ CREATE TABLE IF NOT EXISTS `My_Database`.`cart_has`
   CONSTRAINT `carts_id_fk` FOREIGN KEY (`cart_ID`) REFERENCES cart(`cart_ID`) ON UPDATE CASCADE 
 )ENGINE = InnoDB;
 
+# days avaliable for delivery 
+CREATE TABLE IF NOT EXISTS `My_Database`.`day_avaliable` 
+(
+  `date_time`	DATE NOT NULL, 
+  `slots`       INT NOT NULL, 
+  PRIMARY KEY (`date_time`)
+)ENGINE = InnoDB;
+
+# all orders, their status, and cost
+CREATE TABLE IF NOT EXISTS `My_Database`.`orders` 
+(
+  `order_ID`       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
+  `total_cost`     FLOAT NOT NULL,
+  `is_active`      BOOL NOT NULL,
+  `date_time`      DATE NOT NULL, 
+  PRIMARY KEY (`order_ID`, `date_time`),
+  KEY `date_fk` (`date_time`),
+  CONSTRAINT `date_fk` FOREIGN KEY (`date_time`) REFERENCES day_avaliable (`date_time`) ON UPDATE CASCADE
+)ENGINE = InnoDB;
+
+# who placed an order
+CREATE TABLE IF NOT EXISTS `My_Database`.`placed_order` 
+(
+  `order_ID`       BIGINT UNSIGNED NOT NULL,
+  `email`          VARCHAR(100) NOT NULL, 
+  PRIMARY KEY (`order_ID`,`email`),
+  KEY `order_ID_fk` (`order_ID`),
+  KEY `email_fk` (`email`),
+  CONSTRAINT `email_fk` FOREIGN KEY (`email`) REFERENCES users (`email`) ON UPDATE CASCADE,
+  CONSTRAINT `order_ID_fk` FOREIGN KEY (`order_ID`) REFERENCES orders(`order_ID`) ON UPDATE CASCADE 
+)ENGINE = InnoDB;
+
+
 /*-----------------------------------------------------------------------------*/
 # create a new user 
-
 DROP PROCEDURE IF EXISTS `AddUser`;
 
 DELIMITER $$
@@ -76,10 +125,31 @@ BEGIN
 END $$
 
 DELIMITER ;
+/*-----------------------------------------------------------------------------*/
+# create a admin user
+DROP PROCEDURE IF EXISTS `AddAdmin`;
+
+DELIMITER $$
+USE `My_Database`$$
+CREATE PROCEDURE `AddAdmin`(IN input_email VARCHAR(100), 
+						    IN input_password VARCHAR(500), 
+						    IN input_phone_number VARCHAR(40),
+                            IN input_address VARCHAR (100),
+						    IN input_ig_link VARCHAR(100),
+						    IN input_fb_link  VARCHAR(100),
+			                IN input_twitter_link  VARCHAR(100),
+						    IN input_about VARCHAR(1000))
+BEGIN
+	
+   INSERT INTO `My_Database`.`admin` ( `email`, `user_password`, `phone_number`, `address`,`ig_link`,`fb_link`,`twitter_link`, `about`) 
+   VALUES (input_email, input_password, input_phone_number, input_address, input_ig_link, input_fb_link, input_twitter_link, input_about);
+      
+END $$
+
+DELIMITER ;
 
 /*-----------------------------------------------------------------------------*/
 # create a new product
-
 DROP PROCEDURE IF EXISTS `AddProduct`;
 
 DELIMITER $$
@@ -96,8 +166,8 @@ BEGIN
 END $$
 
 DELIMITER ;
-/*-----------------------------------------------------------------------------*/
 
+/*-----------------------------------------------------------------------------*/
 DROP PROCEDURE IF EXISTS `GetUserInfo`;
 
 DELIMITER $$
@@ -115,20 +185,6 @@ DELIMITER ;
 
 /*-----------------------------------------------------------------------------*/
 # get products
-
-DROP PROCEDURE IF EXISTS `GetProduct`;
-
-DELIMITER $$
-USE `My_Database`$$
-CREATE PROCEDURE `GetProduct`()
-BEGIN
-
-	SELECT * FROM `products`; 
-    
-END $$
-
-DELIMITER ;
-
 DROP PROCEDURE IF EXISTS `GetProduct`;
 
 DELIMITER $$
@@ -143,7 +199,20 @@ END $$
 DELIMITER ;
 
 /*-----------------------------------------------------------------------------*/
+DROP PROCEDURE IF EXISTS `GetProduct`;
 
+DELIMITER $$
+USE `My_Database`$$
+CREATE PROCEDURE `GetProduct`()
+BEGIN
+
+	SELECT * FROM `products`; 
+    
+END $$
+
+DELIMITER ;
+
+/*-----------------------------------------------------------------------------*/
 DROP PROCEDURE IF EXISTS `AddToCart`;
 
 DELIMITER $$
@@ -160,7 +229,6 @@ DELIMITER ;
 
 /*-----------------------------------------------------------------------------*/
 # get number of products
-
 DROP PROCEDURE IF EXISTS `NumberOfProducts`;
 
 DELIMITER $$
@@ -177,7 +245,6 @@ DELIMITER ;
 
 /*-----------------------------------------------------------------------------*/
 # get number of products
-
 DROP PROCEDURE IF EXISTS `ProductQuantity`;
 
 DELIMITER $$
@@ -194,7 +261,6 @@ END $$
 DELIMITER ;
 
 /*-----------------------------------------------------------------------------*/
-
 DROP PROCEDURE IF EXISTS `MakeCart`;
 
 DELIMITER $$
@@ -210,7 +276,36 @@ END $$
 DELIMITER ;
 
 /*-----------------------------------------------------------------------------*/
+DROP PROCEDURE IF EXISTS `AddDay`;
 
+DELIMITER $$
+USE `My_Database`$$
+CREATE PROCEDURE `AddDay`(IN inputDay DATE, IN inputSlots INT)
+BEGIN
+
+   INSERT INTO `My_Database`.`day_avaliable` (`date_time`, `slots`) 
+   VALUES (inputDay, inputSlots);
+      
+END $$
+
+DELIMITER ;
+/*-----------------------------------------------------------------------------*/
+DROP PROCEDURE IF EXISTS `AddOrder`;
+
+DELIMITER $$
+USE `My_Database`$$
+CREATE PROCEDURE `AddOrder`(IN inputDay DATE, IN inputCost FLOAT)
+BEGIN
+
+   INSERT INTO `My_Database`.`orders` (`date_time`, `total_cost`, `is_active`) 
+   VALUES (inputDay, inputCost, true);
+      
+END $$
+
+DELIMITER ;
+
+
+/*-----------------------------------------------------------------------------*/
 DROP FUNCTION IF EXISTS `TotalProducts`;
 
 #*********************************************
@@ -229,4 +324,3 @@ BEGIN
 	RETURN total;
     
 END $$
-
