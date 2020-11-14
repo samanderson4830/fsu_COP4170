2f675efa62f71b87f4de9d733c2278f2cdaf8932
samanderson4830 <50926328+samanderson4830@users.noreply.github.com>
2020-11-13 22:20:06 -0500

    updates to test data

--- db_scripts/test_data.sql
+++ db_scripts/test_data.sql
@@ -1,8 +1,12 @@
+####################################
+# test data for node mysql web app #
+####################################
+
 # make a new user
 call My_Database.AddUser('test@test.com', 'test123', '111-111-1111', '123 street');
 
 # make an admin user
-call My_Database.AddAdmin('admin@admin.com', 'test123', '123-123-1234', '123 admin st. ', 'google.com', 'google.com', 'google.com', 'this is the about summary');
+call My_Database.AddAdmin('admin@admin.com', 'test123', '123-123-1234', '123 admin st.', 'google.com', 'google.com', 'google.com', 'this is the about summary');
 
 # add products to the db
 call My_Database.AddProduct('Product 0', 'This is a product description', 11.99, 10);
@@ -16,16 +20,18 @@ call My_Database.AddProduct('Product 6', 'This is a product description', 9.99, 
 # make a cart
 call My_Database.MakeCart(1);
 
-# add product to cart (cartID , productID, amount)
-call My_Database.AddToCart(1, 1, 4);
+# add product to cart (cartID , productID)
+# call My_Database.AddToCart(1, 1);
 
 # add avaliable days YYYY-MM-DD
-call My_Database.AddDay('2000-11-01', 10);
-call My_Database.AddDay('2000-11-02', 10);
-call My_Database.AddDay('2000-11-03', 10);
-call My_Database.AddDay('2000-11-04', 10);
-call My_Database.AddDay('2000-11-05', 10);
+call My_Database.AddDay('2000-01-01', 10);
+call My_Database.AddDay('2000-02-02', 10);
+call My_Database.AddDay('2000-03-03', 10);
+call My_Database.AddDay('2000-04-04', 10);
+call My_Database.AddDay('2000-05-05', 10);
+
 
 # add an order
-call My_Database.AddOrder('2000-11-01', 12.12);
+call My_Database.AddOrder(1,'2000-05-05', 12.12, "No notes ...");
+
 
