5d266918dcfe47ff6e766c621d62543c74f7770a
samanderson4830 <50926328+samanderson4830@users.noreply.github.com>
2020-11-15 18:54:01 -0500

    PopularProducts view

--- db_scripts/my_database_views.sql
+++ db_scripts/my_database_views.sql
@@ -1,14 +1,23 @@
 /* All views */
 DROP VIEW IF EXISTS `InactiveOrders`;
 
-CREATE  OR REPLACE VIEW InactiveOrders AS
+CREATE OR REPLACE VIEW InactiveOrders AS
 SELECT * 
 FROM orders
 WHERE is_active = 0;
 
 /** new view **/
 DROP VIEW IF EXISTS `ActiveOrders`;
-CREATE  OR REPLACE VIEW ActiveOrders AS
+CREATE OR REPLACE VIEW ActiveOrders AS
 SELECT * 
 FROM orders
 WHERE is_active = 1;
+
+/** new view **/
+DROP VIEW IF EXISTS `PopularProducts`;
+CREATE OR REPLACE VIEW PopularProducts AS
+SELECT product_ID
+FROM order_has
+GROUP BY product_ID
+ORDER BY SUM(amount) DESC;
+    
